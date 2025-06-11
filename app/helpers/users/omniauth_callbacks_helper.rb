542a2288a5cbf2e18a2a2ca574838119caacb53a
Alrasheed-NU <Abdulrahman@u.northwestern.edu>
2025-06-11 12:17:33 -0500

    Implement Google OAuth2 logic and routing

---
+++ app/helpers/users/omniauth_callbacks_helper.rb
@@ -0,0 +1,3 @@
+# In case we want to add helper methods later
+module Users::OmniauthCallbacksHelper
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
+# In app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Add :omniauthable to the list of devise modules
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  #Users can havve many subscriptions
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
   has_many :subscriptions, dependent: :destroy
   has_many :budgets, dependent: :destroy
+
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
