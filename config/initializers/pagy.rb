0187436386352f41b11cc0f388cac1381415dd1f
Alrasheed-NU <Abdulrahman@u.northwestern.edu>
2025-06-11 22:48:46 -0500

    refactor: Update Pagy Initializer Settings

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,10 +1,19 @@
 require 'pagy/extras/bootstrap'
 
-# Set default items per page to 9 (3x3 grid)
-Pagy::DEFAULT[:items] = 9
+# Set default items per page to 10
+Pagy::DEFAULT[:items] = 10
+
+# Explicitly set default limit to 10 as well for consistency
+Pagy::DEFAULT[:limit] = 10
 
 # Set maximum items per page (security measure)
 Pagy::DEFAULT[:max_items] = 49
+
+# Disable items parameter override to enforce the items per page setting
+Pagy::DEFAULT[:items_param] = nil
+
+# Explicitly disable the items extra (which can use session)
+Pagy::DEFAULT[:items_extra] = false
 
 # Enable overflow handling
 # :last_page - redirect to last page if page number is too high
